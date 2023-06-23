cdbc19e9cbe672ec14566259838f68bdd3aeb1c8
Wirsching, Niklas <niklas.wirsching@study.thws.de>
2023-06-23 11:33:54 +0200

    removed files

--- MyIntents/IntentsImports.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import Intents
-
-// Ersetze {ProductModuleName} durch den Namen deines Produktmoduls
-// Normalerweise entspricht der Produktmodulname dem Projektnamen
-@_exported import MyIntents-Swift
