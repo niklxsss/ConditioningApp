e94c6055c555c3fa68c1c1d27f9416d1f736038f
Wirsching, Niklas <niklas.wirsching@study.thws.de>
2023-06-23 11:23:08 +0200

    removed files

--- ShortCutKonditionierung/INShortcut+Extensions.swift
+++
@@ -1,8 +0,0 @@
-/*import Intents
-
-extension INShortcut {
-    var shortcutItem: UIApplicationShortcutItem? {
-        guard let intent = self.intent as? HelloShortcutIntent else { return nil }
-        return UIApplicationShortcutItem(type: HelloShortcutIntent.identifier, localizedTitle: intent.shortcutTitle, localizedSubtitle: nil, icon: UIApplicationShortcutIcon(systemImageName: "gear"), userInfo: nil)
-    }
-}*/
--- ShortCutKonditionierung/NavBar/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  ShortCutKonditionierung
-//
-//  Created by Niklas Wirsching on 26.04.23.
-//
-
-import Foundation
